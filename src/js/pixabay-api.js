71df7f9e29c4abf8c79f6525a0c9c69c815e8834
YaroslawK <xxjokerxx933@gmail.com>
2024-04-18 14:28:15 +0300

    fix

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -6,34 +6,38 @@ import { searchInput } from '../main.js';
 const API_KEY = '43277181-ebb9172f58fa43bc64ca23581';
 const loadButton = document.querySelector('.btn');
 const gallery = document.querySelector('.gallery')
+
 loadButton.addEventListener('click', loadMore);
+
 let page = 1;
 const forma = document.querySelector('.search-form')
 forma.addEventListener('submit', () => {
     page = 1
 });
 
-console.log(page);
-
 export async function fetchImages(searchQuery) {
+   
     const params = new URLSearchParams({
-        key: API_KEY,
+         key: API_KEY,
         q: searchQuery,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
-        per_page: 20,
-        page: page
+        per_page: 15,
+        page
     })
-    
-    
+
+    // const pages = Number(params.get('page'));
+    // const perPage = Number(params.get('per_page'));
+   
+        
     const url = `https://pixabay.com/api/?${params}`;
     try {
         
         const response = await axios(url);
-        // if(response.data.totalHits/per_page === 0){
-        loadButton.classList.replace('btn-hidden', 'btn');
-        // }
+         loadButton.classList.replace('btn-hidden', 'btn');
+        
+
         return response.data.hits;
         }
         catch(error){
@@ -41,12 +45,21 @@ export async function fetchImages(searchQuery) {
         };
 }
 
- async function loadMore() {
-     page += 1;
-    console.log(page);
+async function loadMore() { 
+       page += 1;
      loadButton.disabled = true;
+   
 
     try {
+         const card = document.querySelector('.image');
+    const cardHeight = card.getBoundingClientRect().height;
+    window.scrollBy({
+        left: 0,
+        top: cardHeight * 2,
+        behavior: 'smooth'
+    })
+         
+        
         const searchQuery = searchInput.value.trim();
         const data = await fetchImages(searchQuery);
         gallery.insertAdjacentHTML('beforeend', renderImages(data));
@@ -56,3 +69,4 @@ export async function fetchImages(searchQuery) {
         throw new Error('Failed to fetch images');
     }
 }
+
